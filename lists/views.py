bc0fcab617eab4793341c609c6ede809f3b01aae
sametz <sametz@udel.edu>
2020-06-21 13:57:00 -0400

    Commit prior to "3 strikes" refactor. home.html template has started to use POST data to fill table.

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,3 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,4 +13,9 @@ def home_page(request):
         django.shortcuts.render
 
     """
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        # in order for a 'get' request to pass the test for using the template,
+        # an empty string is needed to be used for the initial page.
+        # Using the .get method allows a default to be returned.
+        'new_item_text': request.POST.get('item_text', '')
+    })
