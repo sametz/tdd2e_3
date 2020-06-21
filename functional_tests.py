11d505fdf974413451689be63a319627bb97541c
sametz <sametz@udel.edu>
2020-06-21 12:20:17 -0400

    End of Ch. 4. Front page HTML now generated from a template.

--- functional_tests.py
+++ functional_tests.py
@@ -34,7 +34,8 @@ class TestNewVisitor(BaseTest):
 
         table = self.driver.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        assert any(row.text == '1: Buy peacock feathers' for row in rows)
+        assert any(row.text == '1: Buy peacock feathers' for row in rows), \
+            "New to-do item did not appear in table"
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
